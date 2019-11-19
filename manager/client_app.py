2fb8c1b09b38d684f9d3d241f730f4a282647a56
zerozero-username <zerozero.username@gmail.com>
2019-11-18 21:49:15 -0300

    changes

--- manager/client_app.py
+++ manager/client_app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from tkinter import Tk, Entry, Button, Label, Scrollbar, Listbox, Grid, StringVar, W, E, S, N
+from tkinter import Tk, Entry, Button, Label, Scrollbar, Listbox, Grid, StringVar, IntVar, W, E, S, N
 
 import tkinter.filedialog
 import tkinter.messagebox
@@ -12,6 +12,53 @@ def client_app_db():
         client_app.title("Client Database")
         client_app.geometry('600x400')
 
-        
+
+        #! Create widgets
+        store_input = StringVar()
+        store_entry = Entry(client_app, textvariable=store_input)
+        store_label = Label(client_app, text='Store', font=('bold', 12))
+
+        store_entry.grid(row=0, column=1, ipady=4, ipadx=4, padx=6, pady=6, sticky=W)
+        store_label.grid(row=0, column=0, padx=2, pady=2)
+
+        store_address_input = StringVar()
+        store_address_entry = Entry(client_app, textvariable=store_address_input)
+        store_address_label = Label(client_app, text='Store Address', font=('bold', 12))
+
+        store_address_entry.grid(row=1, column=1, ipady=4, ipadx=4, padx=6, pady=6, sticky=W)
+        store_address_label.grid(row=1, column=0, padx=2, pady=2)
+
+        store_id_input = IntVar()
+        store_id_entry = Entry(client_app, textvariable=store_id_input)
+        store_id_label = Label(client_app, text='Store ID', font=('bold', 12))
+
+        store_id_entry.grid(row=2, column=1, ipady=4, ipadx=4, padx=6, pady=6, sticky=W)
+        store_id_label.grid(row=2, column=0, padx=2, pady=2)
+
+        city_input = StringVar()
+        city_entry = Entry(client_app, textvariable=city_input)
+        city_label = Label(client_app, text='City', font=('bold', 12))
+
+        city_entry.grid(row=0, column=3, ipady=4, ipadx=4, padx=6, pady=6, sticky=E)
+        city_label.grid(row=0, column=2, padx=2, pady=2)
+
+        postal_code_input = StringVar()
+        postal_code_entry = Entry(client_app, textvariable=postal_code_input)
+        postal_code_label = Label(client_app, text='Postal Code', font=('bold', 12))
+
+        postal_code_entry.grid(row=1, column=3, ipady=4, ipadx=4, padx=6, pady=6, sticky=E)
+        postal_code_label.grid(row=1, column=2, padx=2, pady=2)
+
+        employer_input = StringVar()
+        employer_entry = Entry(client_app, textvariable=employer_input)
+        employer_label = Label(client_app, text='Employer', font=('bold', 12))
+
+        employer_entry.grid(row=2, column=3, ipady=4, ipadx=4, padx=6, pady=6, sticky=E)
+        employer_label.grid(row=2, column=2, padx=2, pady=2)
+
+        #! Listboxes
+        store_list = Listbox(client_app, height=12, width=42)
+        store_list.grid(client_app, row=3, column=0, columnspan=3, rowspan=3)
+
 
         client_app.mainloop()
--- manager/client_db.py
+++ manager/client_db.py
@@ -7,5 +7,5 @@ class client_database():
     def __init__(self, client_data):
         self.connection = sqlite3.connect(client_data)
         self.cursor = self.connection.cursor()
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS client_database (client_id INTEGER PRIMARY KEY, store text, store_address text, store_id int, city text, postal_code int, employer text)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS client_database (client_id INTEGER PRIMARY KEY, store text, store_address text, store_id int, city text, zip_code int, employer text)")
         self.connection.commit()
