ef32cc575bf38bf0678d744244991855e3820720
zerozero-username <zerozero.username@gmail.com>
2019-11-18 22:25:50 -0300

    changes

--- manager/retailer_app.py
+++ manager/retailer_app.py
@@ -13,51 +13,44 @@ def retailer_app_db():
         retailer_app.geometry('650x400')
 
         #! Create widgets
-        store_input = StringVar()
-        store_entry = Entry(retailer_app, textvariable=store_input)
-        store_label = Label(retailer_app, text='Store', font=('bold', 12))
+        brand_input = StringVar()
+        brand_entry = Entry(retailer_app, textvariable=brand_input)
+        brand_label = Label(retailer_app, text='Brand', font=('bold', 12))
 
-        store_entry.grid(row=0, column=1, ipady=4, ipadx=4, padx=6, pady=6)
-        store_label.grid(row=0, column=0, padx=2, pady=2)
+        brand_entry.grid(row=0, column=1, ipady=4, ipadx=4, padx=6, pady=6)
+        brand_label.grid(row=0, column=0, padx=2, pady=2)
 
-        store_address_input = StringVar()
-        store_address_entry = Entry(retailer_app, textvariable=store_address_input)
-        store_address_label = Label(retailer_app, text='Store Address', font=('bold', 12))
+        brand_id_input = IntVar()
+        brand_id_entry = Entry(retailer_app, textvariable=brand_id_input)
+        brand_id_label = Label(retailer_app, text='ID', font=('bold', 12))
 
-        store_address_entry.grid(row=1, column=1, ipady=4, ipadx=4, padx=6, pady=6)
-        store_address_label.grid(row=1, column=0, padx=2, pady=2)
+        brand_id_entry.grid(row=1, column=1, ipady=4, ipadx=4, padx=6, pady=6)
+        brand_id_label.grid(row=1, column=0, padx=2, pady=2)
 
-        store_id_input = IntVar()
-        store_id_entry = Entry(retailer_app, textvariable=store_id_input)
-        store_id_label = Label(retailer_app, text='ID', font=('bold', 12))
+        product_input = StringVar()
+        product_entry = Entry(retailer_app, textvariable=product_input)
+        product_label = Label(retailer_app, text='Product', font=('bold', 12))
 
-        store_id_entry.grid(row=2, column=1, ipady=4, ipadx=4, padx=6, pady=6)
-        store_id_label.grid(row=2, column=0, padx=2, pady=2)
+        product_entry.grid(row=0, column=3, ipady=4, ipadx=4, padx=6, pady=6)
+        product_label.grid(row=0, column=2, padx=2, pady=2)
 
-        city_input = StringVar()
-        city_entry = Entry(retailer_app, textvariable=city_input)
-        city_label = Label(retailer_app, text='City', font=('bold', 12))
+        Category_input = StringVar()
+        Category_entry = Entry(retailer_app, textvariable=Category_input)
+        Category_label = Label(retailer_app, text='Category', font=('bold', 12))
 
-        city_entry.grid(row=0, column=3, ipady=4, ipadx=4, padx=6, pady=6)
-        city_label.grid(row=0, column=2, padx=2, pady=2)
+        Category_entry.grid(row=1, column=3, ipady=4, ipadx=4, padx=6, pady=6)
+        Category_label.grid(row=1, column=2, padx=2, pady=2)
 
-        postal_code_input = StringVar()
-        postal_code_entry = Entry(retailer_app, textvariable=postal_code_input)
-        postal_code_label = Label(retailer_app, text='Postal Code', font=('bold', 12))
+        stock_input = IntVar()
+        stock_entry = Entry(retailer_app, textvariable=stock_input)
+        stock_label = Label(retailer_app, text='Stock', font=('bold', 12))
 
-        postal_code_entry.grid(row=1, column=3, ipady=4, ipadx=4, padx=6, pady=6)
-        postal_code_label.grid(row=1, column=2, padx=2, pady=2)
-
-        employer_input = StringVar()
-        employer_entry = Entry(retailer_app, textvariable=employer_input)
-        employer_label = Label(retailer_app, text='Employer', font=('bold', 12))
-
-        employer_entry.grid(row=2, column=3, ipady=4, ipadx=4, padx=6, pady=6)
-        employer_label.grid(row=2, column=2, padx=2, pady=2)
+        stock_entry.grid(row=2, column=3, ipady=4, ipadx=4, padx=6, pady=6)
+        stock_label.grid(row=2, column=2, padx=2, pady=2)
 
         #! Listboxes
-        store_info = Listbox(retailer_app, height=10, width=82)
-        store_info.grid(row=5, column=0, columnspan=4, ipady=4, ipadx=4, padx=6, pady=6)
+        brand_info = Listbox(retailer_app, height=10, width=82)
+        brand_info.grid(row=5, column=0, columnspan=4, ipady=4, ipadx=4, padx=6, pady=6)
 
         retailer_app.mainloop()
 
